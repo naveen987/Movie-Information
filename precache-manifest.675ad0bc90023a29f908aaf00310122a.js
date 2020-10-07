ec2c69bdcf4d236916539c9f788128ad4169a639
naveen987 <tsnaveentumkur@gmail.com>
2020-10-08 00:32:11 +0530

    Updates

---
+++ precache-manifest.675ad0bc90023a29f908aaf00310122a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043235f8f5a7ec3c11f501fc8328a98f",
+    "url": "/Movie-Information/index.html"
+  },
+  {
+    "revision": "2708aaf420b395c75db4",
+    "url": "/Movie-Information/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "0939055b657bd422635a",
+    "url": "/Movie-Information/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2708aaf420b395c75db4",
+    "url": "/Movie-Information/static/js/2.8773bf50.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Movie-Information/static/js/2.8773bf50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0939055b657bd422635a",
+    "url": "/Movie-Information/static/js/main.7543065a.chunk.js"
+  },
+  {
+    "revision": "f305433029f0bd5cbe57",
+    "url": "/Movie-Information/static/js/runtime-main.e564c338.js"
+  }
+]);
